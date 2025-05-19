492e65e562222509d41e7f1b937058c4b30a292b
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2025-05-19 12:00:37 +0200

    Add timing for data uncompressed generation

---
+++ data_RAW_uncompressed_TTree.py
@@ -0,0 +1,42 @@
+import FWCore.ParameterSet.Config as cms
+
+import sys
+
+if len(sys.argv) < 2:
+    raise RuntimeError("Usage: cmsRun step3_PARKING.py num_events")
+
+if int(sys.argv[1]) < 0:
+    raise RuntimeError(f"Number of events to skip must be positive. Input was {sys.argv[1]}")
+
+num_events = int(sys.argv[1])
+
+process = cms.Process("SPLIT")
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:../store/772d714f-fda4-4a82-a98d-e1a07d65d36a.root'),
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(num_events),
+    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+)
+
+# Output definition
+
+process.RAWRNTupleoutput = cms.OutputModule("PoolOutputModule",
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(0),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM-DIGI-RAW'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string(f'file:../store/uncompressed/data_RAW_TTree_{num_events}.root'),
+    outputCommands = cms.untracked.vstring(
+    'drop *',
+    'keep FEDRawDataCollection_rawDataCollector_*_*'
+    )
+)
+
+process.options.numberOfThreads = 30
+
+process.RNTupleRAWoutput_step = cms.EndPath(process.RAWRNTupleoutput)
---
+++ data_uncompressed_generation.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Output CSV file
+output_file="../store/uncompressed/data_timing_results.csv"
+echo "events,format,time_seconds" > "$output_file"
+
+# List of event counts
+for events in 1 10 100 1000 5000; do
+    echo "Running with $events events in TTree format ..."
+
+    log_file="../store/uncompressed/log_data_RAW_uncompressed_TTree_${events}.txt"
+
+    # Run command, save log, and time it
+    time_output=$( ( /usr/bin/time -f "%e" cmsRun data_RAW_uncompressed_TTree.py "$events" ) &> "$log_file" 2>&1 )
+    run_time=$(tail -1 $log_file)
+
+    # Save timing to CSV
+    echo "$events,TTree,$run_time" >> "$output_file"
+
+    # Notify completion
+    notify-complete "data_RAW_uncompressed_TTree.py for "$events" events completed in "$run_time" seconds."
+
+
+
+    echo "Running with $events events in RNTuple format ..."
+
+    log_file="../store/uncompressed/log_data_RAW_uncompressed_RNTuple_${events}.txt"
+
+    # Run command, save log, and time it
+    time_output=$( ( /usr/bin/time -f "%e" cmsRun data_RAW_uncompressed_RNTuple.py "$events" ) &> "$log_file" 2>&1 )
+    run_time=$(tail -1 $log_file)
+    # Save timing to CSV
+    echo "$events,RNTuple,$run_time" >> "$output_file"
+
+    # Notify completion
+    notify-complete "data_RAW_uncompressed_RNTuple.py for "$events" events completed in "$run_time" seconds."
+
+done
