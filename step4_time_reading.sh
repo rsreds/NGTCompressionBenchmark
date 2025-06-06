96035d3d1df3b39736a549b89a7feedcdf395c7a
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2025-06-06 10:58:58 +0200

    Update timing plots

--- step4_time_reading.sh
+++ step4_time_reading.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 # Output CSV file
-output_file="../results/fixed_mc_timing_results_reading.csv"
-# echo "events,format,time_seconds" > "$output_file"
+output_file="../results/mc_timing_results_reading.csv"
+echo "events,format,time_seconds" > "$output_file"
 
 # List of event counts
-for events in 1 10 100 1000 2000 3000 4000 5000; do
-    for format in TTree RNTuple; do
+for format in TTree RNTuple; do
+    for events in 1 10 100 1000 2000 3000 4000 5000; do
+        # Do for 3 runs and discard for caching, then do 10 runs and average the time
+        echo "Running with $events events in $format format (caching runs)..."
+        for run in {1..3}; do
+            echo "Running with $events events in $format format (caching run $run/3)..."
+            filename="mc_RAW_${format}_${events}.root"
+            # Run command, save log, and time it
+            cmsRun step4_GenericConsumer_${format}.py "$filename" "$events" > /dev/null 2>&1
+        done
         total_time=0
         for run in {1..10}; do
             echo "Running with $events events in $format format (run $run/10)..."
             log_file="../store/uncompressed/logs/log_step4_GenericConsumer_${format}_${events}_run${run}.txt"
-            filename="mc_RAW_${format}_5000.root"
+            filename="mc_RAW_${format}_${events}.root"
             # Run command, save log, and time it
             run_time=$( /usr/bin/time -f "%e" cmsRun step4_GenericConsumer_${format}.py "$filename" "$events" 2>&1 | tail -1 )
             echo "$run_time" > "$log_file"
--- timing_plot.py
+++ timing_plot.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 
 #type,events,format,algo,level,time_seconds,filesize_byte
 
-reading_timing = "../results/fixed_mc_timing_results_reading.csv"
-generation_timing = "../results/mc_timing_results_generation.csv"
+reading_timing = "../results/mc_timing_results_reading.csv"
+generation_timing = "../results/timing_results_generation.csv"
 
 df_reading = pd.read_csv(reading_timing, sep=',', header=0)
 df_generation = pd.read_csv(generation_timing, sep=',', header=0)
-df_uncompressed = pd.read_csv(uncompressed_timing, sep=',', header=0)
+# df_uncompressed = pd.read_csv(uncompressed_timing, sep=',', header=0)
 
 colors = ['#5790fc', '#f89c20', '#e42536', '#7021dd']
 
@@ -28,7 +28,7 @@ y = np.array(df_reading[df_reading['format']=='RNTuple']['time_seconds'])
 p = np.polyfit(x, y, 1)
 ax.plot(x, p[0] * x + p[1], color=colors[1], linestyle='--', label='RNTuple fit')
 
-plt.savefig("mc_reading_timing.png")
+plt.savefig("../results/plots/mc_reading_timing.png")
 
 fig, ax = plt.subplots(figsize=(10, 6))
 df_generation[df_generation['format']=='TTree'].plot(x='events', y='time_seconds', kind='scatter', color=colors[0], label='TTree', marker='o', ax=ax)
@@ -44,5 +44,5 @@ x = np.array(df_generation[df_generation['format']=='RNTuple']['events'])
 y = np.array(df_generation[df_generation['format']=='RNTuple']['time_seconds'])
 p = np.polyfit(x, y, 1)
 ax.plot(x, p[0] * x + p[1], color=colors[1], linestyle='--', label='RNTuple fit')
-plt.savefig("mc_generation_timing.png")
+plt.savefig("../results/plots/mc_generation_timing.png")
 
